2365ea1a76d1503ac48bbdb6cded23ae960cfeb2
evertonse <evertonse.junior@gmail.com>
2023-07-03 03:44:58 -0300

    better asm

--- src/ihs/project1/evertonsantos_202100011379_sort.c
+++ src/ihs/project1/evertonsantos_202100011379_sort.c
@@ -1,6 +1,7 @@
 // Como executar no terminal:
-// gcc -Wall -O3 nomesobrenome_123456789012_exemplo.c -o nomesobrenome_123456789012_exemplo
-// gcc -Wall -O3 project.c -o project.bin && ./project.bin sort.input
+// gcc -Wall -O3 nomesobrenome_123456789012_exemplo.c -o
+// nomesobrenome_123456789012_exemplo gcc -Wall -O3 project.c -o project.bin &&
+// ./project.bin sort.input
 // ./nomesobrenome_123456789012_exemplo entrada saida
 
 // Numeros padronizados
@@ -16,96 +17,94 @@ int a = 'd';
 #include <stdlib.h>
 
 void bubble_sort(int arr[], int n) {
-    int i, j;
-    for (i = 0; i < n-1; i++) {
-        for (j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
+  int i, j;
+  for (i = 0; i < n - 1; i++) {
+    for (j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
     }
+  }
 }
-
 
 void go_sort(int);
 
 FILE *input, *output;
 
-int main(int argc, char* argv[]) {
-    input = fopen(argv[1], "r");
-    if (input == NULL) {
-        printf("Failed to open the input %s\n",argv[1]);
-        return 1;
-    }
+int main(int argc, char *argv[]) {
+  input = fopen(argv[1], "r");
+  if (input == NULL) {
+    printf("Failed to open the input %s\n", argv[1]);
+    return 24;
+  }
 
-    output = fopen(argv[2], "w");
-    if (output == NULL) {
-        printf("Failed to open the output %s.\n", argv[2]);
-        return 1;
-    }
+  output = fopen(argv[2], "w");
+  if (output == NULL) {
+    printf("Failed to open the output %s.\n", argv[2]);
+    return 420;
+  }
 
-    int n_array = 0;
-    if (fscanf(input, "%d\n", &n_array) != 1) {
-       printf("Invalid input format.\n");
-       return 1;
-    }
-    for (size_t i = 0; i < n_array; i++) {
-        go_sort(i);
-    }
+  int n_array = 0;
+  if (fscanf(input, "%d\n", &n_array) != 1) {
+    printf("Invalid input format.\n");
+    return 69;
+  }
+  for (size_t i = 0; i < n_array; i++) {
+    go_sort(i);
+  }
 
-    fclose(input);
-    fclose(output);
+  fclose(input);
+  fclose(output);
 
-    return 1;
+  return 0;
 }
 
 void go_sort(int run) {
+  int *numbers;
+  int count = 0;
+  // Read the first number in the input as the size of the array
+  if (fscanf(input, "%d\n", &count) != 1) {
+    printf("Invalid input format.\n");
+    exit(1);
+  }
 
-    int *numbers;
-    int count = 0;
-    // Read the first number in the input as the size of the array
-     if (fscanf(input, "%d\n", &count) != 1) {
-        printf("Invalid input format.\n");
-        exit(1);
+  // Allocate memory for the array dynamically
+  numbers = (int *)malloc(count * sizeof(int));
+  if (numbers == NULL) {
+    printf("Memory allocation failed.\n");
+    exit(1);
+  }
+
+  // Read numbers from the input
+
+  printf("################################\n");
+  printf("--------------INPUT---------------\n");
+  printf("[%d] ", run);
+  for (int i = 0; i < count; i++) {
+    if (fscanf(input, "%d", &numbers[i]) != 1) {
+      printf("Invalid input format.\n");
+      exit(1);
     }
+    printf("%d ", numbers[i]);
+  }
+  printf("\n");
+  // Sort the numbers using bubble sort
+  bubble_sort(numbers, count);
 
-    // Allocate memory for the array dynamically
-    numbers = (int*)malloc(count * sizeof(int));
-    if (numbers == NULL) {
-        printf("Memory allocation failed.\n");
-        exit(1);
-    }
+  printf("---------------OUTPUT---------------\n");
+  // Display the sorted numbers
+  fprintf(output, "[%d] ", run);
+  printf("[%d] ", run);
+  for (int i = 0; i < count; i++) {
+    fprintf(output, "%d ", numbers[i]);
+    printf("%d ", numbers[i]);
+  }
+  fprintf(output, "\n");
+  printf("\n");
 
-    // Read numbers from the input
-
-    printf("################################\n");
-    printf("--------------INPUT---------------\n");
-    printf("[%d] ", run);
-    for (int i = 0; i < count; i++) {
-        if (fscanf(input, "%d", &numbers[i]) != 1) {
-            printf("Invalid input format.\n");
-            exit(1);
-        }
-        printf("%d ", numbers[i]);
-    }
-    printf("\n");
-    // Sort the numbers using bubble sort
-    bubble_sort(numbers, count);
-
-    printf("---------------OUTPUT---------------\n");
-    // Display the sorted numbers
-    fprintf(output, "[%d] ", run);
-    printf("[%d] ", run);
-    for (int i = 0; i < count; i++) {
-        fprintf(output, "%d ", numbers[i]);
-        printf("%d ", numbers[i]);
-    }
-    fprintf(output, "\n");
-    printf("\n");
-
-    // Free the dynamically allocated memory
-    free(numbers);
-    // Close the input
+  // Free the dynamically allocated memory
+  free(numbers);
+  // Close the input
 }
--- src/ihs/project1/run.sh
+++ src/ihs/project1/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 gcc -Wall -O3 -g evertonsantos_202100011379_sort.c -o bin
 gcc -Wall -Os -c evertonsantos_202100011379_sort.c -o bin.o
-gcc -Wall -Os -S -masm=intel evertonsantos_202100011379_sort.c -o evertonsantos_202100011379_sort.s
-./bin sort.input sort.output
+gcc -Wall -Os -S -masm=intel evertonsantos_202100011379_sort.c -o evertonsantos_202100011379_sort.gcc.s
+./bin sort.input sort.gcc.output
 objump='objdump -M intel'
 objdump -d bin > bin.dump
 objdump -d bin.o > bin.o.dump
-gcc -Wall -O3 evertonsantos_202100011379_sort.work.s -o work.bin 
+gcc -Wall -O3 evertonsantos_202100011379_sort.s -o work.bin 
